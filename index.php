8a050af53414d16438f9977882ca39ed9baaa1bb
kellerflint <kflint0068@gmail.com>
2020-02-24 11:44:02 -0800

    index page inline comments

--- index.php
+++ index.php
@@ -1,10 +1,9 @@
 <?php
-//session_start();
+
 //require the autoload file
 require_once('vendor/autoload.php');
 require_once('model/validation.php');
 
-//THIS IS OUR CONTROLLER
 //Turn on error reporting
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
@@ -18,35 +17,37 @@ $f3 = Base::instance();
 $db = new Database();
 $controller = new Controller($f3);
 
-//Define a default route
-//call a function which gets the projects form database
+// Define a default route
 $f3->route('GET /', function () {
     global $controller;
     $controller->projectsPage();
 });
 
-
+// Define video player route
 $f3->route('GET /player/@item', function ($f3, $param) {
     global $controller;
     $controller->videoPlayer($param);
 });
 
-//setting route for sessions page
+// Define sessions page route
 $f3->route('GET /sessions', function () {
     global $controller;
     $controller->sessionsPage();
 });
 
+// Define login route
 $f3->route('GET|POST /login', function() {
     global $controller;
     $controller->loginPage();
 });
 
+// Define logout route
 $f3->route('GET|POST /logout', function() {
     global $controller;
     $controller->logout();
 });
 
+// Define session-edit route
 $f3->route('GET|POST /session-edit/@id', function($f3, $param) {
     global $controller;
     $controller->editSessionPage($param);
